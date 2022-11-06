3d7524201330304a2d6ed8ebca0f353d09442c1c
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-06 13:51:28 -0300

    feat: testando tipos

--- types/types.js
+++ types/types.js
@@ -3,3 +3,12 @@ let nome = 'João';
 console.log(nome);
 // descomentar para teste:
 // nome = 28;
+//numbers
+let idade = 27;
+// idade = 'João';
+idade = 27.5;
+console.log(idade);
+// bolean
+let possuiHobbies = true;
+// teste = 1
+console.log(possuiHobbies);
--- types/types.ts
+++ types/types.ts
@@ -3,3 +3,15 @@ console.log(nome);
 
 // descomentar para teste:
 // nome = 28;
+
+//numbers
+let idade = 27;
+// idade = 'João';
+idade = 27.5;
+
+console.log(idade);
+
+// bolean
+let possuiHobbies = true;
+// teste = 1
+console.log(possuiHobbies);
