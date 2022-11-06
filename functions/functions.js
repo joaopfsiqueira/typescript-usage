ee61a41e179e10033ae10fb921d85db5a2d69d8a
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-06 15:31:40 -0300

    feat: testando function explicita

--- functions/functions.js
+++ functions/functions.js
@@ -1,7 +1,10 @@
 "use strict";
 //funções
 // Sim!! Ele está pegando a variável "nome", setada no arquivo /types/type.ts! É uma das maravilhas do tsc -w
+// tipando variável, ou certo, tipando como deve ser o retorno dela!
+// para testar, basta colocar um retorno com tipoo diferente.
 function retornaMeuNome() {
     return nome;
+    //   return minhaIdade
 }
 console.log(retornaMeuNome());
--- functions/functions.ts
+++ functions/functions.ts
@@ -1,7 +1,10 @@
 //funções
 // Sim!! Ele está pegando a variável "nome", setada no arquivo /types/type.ts! É uma das maravilhas do tsc -w
-function retornaMeuNome() {
+// tipando variável, ou certo, tipando como deve ser o retorno dela!
+// para testar, basta colocar um retorno com tipoo diferente.
+function retornaMeuNome(): string {
   return nome;
+  //   return minhaIdade
 }
 
 console.log(retornaMeuNome());
