7f4d2a957a761405c4936db4a9edce6c91c35e14
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-06 17:48:13 -0300

    feat: aprofundando funções

--- functions/functions.js
+++ functions/functions.js
@@ -19,3 +19,26 @@ function multiplicar(numA, numB) {
     return numA * numB;
 }
 console.log(multiplicar(2, 4));
+//outro exemplo explicitando os parametros como number e informando que a função vai retornar boolean.
+const teste = function (a, b) {
+    if (a + b == 5) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+console.log(teste(3, 2));
+//tipo função, atribuindo valor de uma função em uma outra. Colocando o mouse em cima, vemos que retorna "any"
+let calculo;
+calculo = digaOi;
+calculo();
+// ou certo, nesses exemplos, não deu nenhum erro pq está como any!
+calculo = multiplicar;
+console.log(calculo(5, 6));
+// desse outro jeito, vamos setar uma variável com o tipo função e todo o resto!
+// dessa forma, criamos uma variável e transformamos ela em uma função que vai retornar number e seus parametros.
+let calculo2;
+calculo2 = multiplicar;
+// calculo2 = digaOi          -> demonstrando erro
+console.log(calculo2(5, 10));
--- functions/functions.ts
+++ functions/functions.ts
@@ -23,3 +23,30 @@ function multiplicar(numA: number, numB: number): number {
 }
 
 console.log(multiplicar(2, 4));
+
+//outro exemplo explicitando os parametros como number e informando que a função vai retornar boolean.
+const teste = function (a: number, b: number): boolean {
+  if (a + b == 5) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(teste(3, 2));
+
+//tipo função, atribuindo valor de uma função em uma outra. Colocando o mouse em cima, vemos que retorna "any"
+let calculo;
+calculo = digaOi;
+calculo();
+
+// ou certo, nesses exemplos, não deu nenhum erro pq está como any!
+calculo = multiplicar;
+console.log(calculo(5, 6));
+
+// desse outro jeito, vamos setar uma variável com o tipo função e todo o resto!
+// dessa forma, criamos uma variável e transformamos ela em uma função que vai retornar number e seus parametros.
+let calculo2: (numeroA: number, numeroB: number) => number;
+calculo2 = multiplicar;
+// calculo2 = digaOi          -> demonstrando erro
+console.log(calculo2(5, 10));
