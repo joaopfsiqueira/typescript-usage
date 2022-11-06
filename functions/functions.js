4854d7f9e1742a77e284af88e01690407a20b433
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-06 15:30:33 -0300

    feat: functions

--- functions/functions.js
+++ functions/functions.js
@@ -1,2 +1,7 @@
 "use strict";
 //funções
+// Sim!! Ele está pegando a variável "nome", setada no arquivo /types/type.ts! É uma das maravilhas do tsc -w
+function retornaMeuNome() {
+    return nome;
+}
+console.log(retornaMeuNome());
--- functions/functions.ts
+++ functions/functions.ts
@@ -1,1 +1,7 @@
 //funções
+// Sim!! Ele está pegando a variável "nome", setada no arquivo /types/type.ts! É uma das maravilhas do tsc -w
+function retornaMeuNome() {
+  return nome;
+}
+
+console.log(retornaMeuNome());
