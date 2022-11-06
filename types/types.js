fdfdffed025003664fecdb31518443e043bf6998
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-06 13:55:19 -0300

    feat: explicitando valores.

--- types/types.js
+++ types/types.js
@@ -12,3 +12,12 @@ console.log(idade);
 let possuiHobbies = true;
 // teste = 1
 console.log(possuiHobbies);
+// tipos explícitos
+let minhaIdade;
+minhaIdade = 22;
+console.log(typeof minhaIdade);
+minhaIdade = 'idade é 22';
+console.log(typeof minhaIdade);
+// nesse exemplo ele não retorna nenhum erro, uma vez que não declaramos o tipo de nenhuma maneira.
+// seja tipando ou atribuindo um valor padrão à variável.
+// e ainda informei um "any", avisando que pode ser qualquer valor.
--- types/types.ts
+++ types/types.ts
@@ -1,17 +1,27 @@
-let nome = 'João';
+let nome: string = 'João';
 console.log(nome);
 
 // descomentar para teste:
 // nome = 28;
 
 //numbers
-let idade = 27;
+let idade: number = 27;
 // idade = 'João';
 idade = 27.5;
 
 console.log(idade);
 
 // bolean
-let possuiHobbies = true;
+let possuiHobbies: boolean = true;
 // teste = 1
 console.log(possuiHobbies);
+
+// tipos explícitos
+let minhaIdade: any;
+minhaIdade = 22;
+console.log(typeof minhaIdade);
+minhaIdade = 'idade é 22';
+console.log(typeof minhaIdade);
+// nesse exemplo ele não retorna nenhum erro, uma vez que não declaramos o tipo de nenhuma maneira.
+// seja tipando ou atribuindo um valor padrão à variável.
+// e ainda informei um "any", avisando que pode ser qualquer valor.
