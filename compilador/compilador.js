6b4d2e29fed21fa8c81bf1f6e759e84596bdccf2
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-13 12:36:24 -0300

    feat: testando erro

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -1,7 +1,6 @@
-let canal: string = 'Gaveta';
-let inscritos: number = 6103;
-
+"use strict";
+let canal = 'Gaveta';
+let inscritos = 6103;
 // é para dar erro, uma vez que uma variável é string e a outra não
 canal = inscritos;
-
 console.log(canal);
