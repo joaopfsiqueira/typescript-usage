59ff1d5f8e4b98893eead7f6fe3d1b7db1d877ee
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-13 12:43:18 -0300

    feat: ajuste exemplo compilador

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -2,5 +2,6 @@
 let canal = 'Gaveta';
 let inscritos = 6103;
 // é para dar erro, uma vez que uma variável é string e a outra não
-canal = inscritos;
+// testando "noEmitOnError": true, basta excluir o compilador.js e descomentar o erro!
+// canal = inscritos;
 console.log(canal);
--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -2,6 +2,7 @@ let canal: string = 'Gaveta';
 let inscritos: number = 6103;
 
 // é para dar erro, uma vez que uma variável é string e a outra não
-canal = inscritos;
+// testando "noEmitOnError": true, basta excluir o compilador.js e descomentar o erro!
+// canal = inscritos;
 
 console.log(canal);
