bc72d89c409914b3dc8327ef46a6884e9a92b831
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-14 09:12:14 -0300

    feat: functions anonimas ou não

--- build/ecmascript/arrowFunction.js
+++ build/ecmascript/arrowFunction.js
@@ -1,7 +1,14 @@
 "use strict";
 // Arrow Function
+// duas maneiras de se declarar function de forma padrão, anonimamente ou não.
+// Esse jeito abaixo define uma função anonima e salva ela dentro de uma variável.
 const somar = function (n1, n2) {
     return n1 + n2;
 };
+console.log(somar(2, 2));
+// ou então, basta declarar um nome na função. O resultado é literalmente o mesmo.
+function somar2(n1, n2) {
+    return n1 + n2;
+}
 console.log(somar(2, 2));
 //# sourceMappingURL=arrowFunction.js.map
--- ecmascript/arrowFunction.ts
+++ ecmascript/arrowFunction.ts
@@ -1,6 +1,16 @@
 // Arrow Function
+// duas maneiras de se declarar function de forma padrão, anonimamente ou não.
+
+// Esse jeito abaixo define uma função anonima e salva ela dentro de uma variável.
 const somar = function (n1: number, n2: number): number {
   return n1 + n2;
 };
+
+console.log(somar(2, 2));
+
+// ou então, basta declarar um nome na função. O resultado é literalmente o mesmo.
+function somar2(n1: number, n2: number): number {
+  return n1 + n2;
+}
 
 console.log(somar(2, 2));
