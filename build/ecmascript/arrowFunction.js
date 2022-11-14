2f4d903b895f08bf66353b134c4a3a59b21a5929
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-14 09:10:46 -0300

    feat: arrowFunction

---
+++ build/ecmascript/arrowFunction.js
@@ -0,0 +1,7 @@
+"use strict";
+// Arrow Function
+const somar = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(somar(2, 2));
+//# sourceMappingURL=arrowFunction.js.map
---
+++ ecmascript/arrowFunction.ts
@@ -0,0 +1,6 @@
+// Arrow Function
+const somar = function (n1: number, n2: number): number {
+  return n1 + n2;
+};
+
+console.log(somar(2, 2));
