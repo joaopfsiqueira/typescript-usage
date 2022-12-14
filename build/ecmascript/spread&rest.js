c6d06b0e4ad61f7d1b5d870aef3ad74af8481261
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-12-14 10:46:34 -0300

    feat: melhorando spread operator

--- build/ecmascript/spread&rest.js
+++ build/ecmascript/spread&rest.js
@@ -5,4 +5,7 @@ console.log(Math.max(...numbers));
 // mas o que o spread operator agrega? Bom, sem ele ficaria dessa forma:
 console.log(Math.max(numbers[0], numbers[1], numbers[2], numbers[3]));
 // então, basicamente ele instância cada elemento do array e passa como parâmetro como da forma acima, só que vem mais clean!
+const turmaA = ['João', 'Maria', 'Fernanda'];
+const turmaB = ['Fernando', 'Miguel', 'Lorena', ...turmaA]; //pegando todos os elementos da turma A e jogando na turma B com spread.
+console.log(turmaB);
 //# sourceMappingURL=spread&rest.js.map
--- ecmascript/spread&rest.ts
+++ ecmascript/spread&rest.ts
@@ -7,3 +7,8 @@ console.log(Math.max(...numbers));
 console.log(Math.max(numbers[0], numbers[1], numbers[2], numbers[3]));
 
 // então, basicamente ele instância cada elemento do array e passa como parâmetro como da forma acima, só que vem mais clean!
+
+const turmaA: string[] = ['João', 'Maria', 'Fernanda'];
+const turmaB: string[] = ['Fernando', 'Miguel', 'Lorena', ...turmaA]; //pegando todos os elementos da turma A e jogando na turma B com spread.
+
+console.log(turmaB);
