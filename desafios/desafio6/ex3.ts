010c0741d3d11ab796d3d2b3cdcc34921778d15b
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-01-25 11:49:44 -0300

    feat: desafio 6, ex 3

--- desafios/desafio6/ex3.ts
+++ desafios/desafio6/ex3.ts
@@ -1,25 +1,49 @@
 // Exercício 3 - Getters & Setters
-var estagiario = {
-  _primeiroNome: '',
-};
 
-Object.defineProperty(estagiario, 'primeiroNome', {
-  get: function () {
+// Usar getter and setters de typescript para refatorar o código abaixo
+// var estagiario = {
+//   _primeiroNome: '',
+// };
+
+// Object.defineProperty(estagiario, 'primeiroNome', {
+//   get: function () {
+//     return this._primeiroNome;
+//   },
+//   set: function (valor) {
+//     if (valor.length >= 3) {
+//       this._primeiroNome = valor;
+//     } else {
+//       this._primeiroNome = '';
+//     }
+//   },
+//   enumerable: true,
+//   configurable: true,
+// });
+
+// console.log(estagiario.primeiroNome);
+// estagiario.primeiroNome = 'Le';
+// console.log(estagiario.primeiroNome);
+// estagiario.primeiroNome = 'Leonardo';
+// console.log(estagiario.primeiroNome);
+
+class Estagiario {
+  public _primeiroNome: string = '';
+
+  get primeiroNome(): string {
     return this._primeiroNome;
-  },
-  set: function (valor) {
-    if (valor.length >= 3) {
-      this._primeiroNome = valor;
+  }
+
+  set primeiroNome(nome: string) {
+    if (nome.length >= 3) {
+      this._primeiroNome = nome;
+      console.log(this._primeiroNome);
+      console.log(this.primeiroNome);
     } else {
       this._primeiroNome = '';
     }
-  },
-  enumerable: true,
-  configurable: true,
-});
+  }
+}
 
-console.log(estagiario.primeiroNome);
-estagiario.primeiroNome = 'Le';
-console.log(estagiario.primeiroNome);
-estagiario.primeiroNome = 'Leonardo';
-console.log(estagiario.primeiroNome);
+const estagiario1 = new Estagiario();
+estagiario1.primeiroNome = 'João';
+console.log(estagiario1);
