2002b3fadd033653ace35de12a515aa9f730827b
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-01-30 16:25:45 -0300

    feat: montando exemplo de namespace aninhados

---
+++ src/namespaces/namespacesMultiplosArquivos/geometriaLoja.ts
@@ -0,0 +1,9 @@
+namespace McDonalds {
+  export namespace Loja {
+    const valor = 20000;
+
+    export function vender(): string {
+      return `compre a loja por ${valor}`;
+    }
+  }
+}
---
+++ src/namespaces/namespacesMultiplosArquivos/principal.ts
@@ -0,0 +1,4 @@
+const quantidade = 2;
+
+console.log(McDonalds.Loja.adquirir(quantidade));
+console.log(McDonalds.Loja.vender());
