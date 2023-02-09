a8a0f907a6b7353892871ebe6c6aae54862d7952
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-02-09 09:16:28 -0300

    feat: começando módulos

--- src/namespaces/namespaces.ts
+++ src/namespaces/namespaces.ts
@@ -2,10 +2,10 @@
 // Ou certo, em typescript não podemos criar funções ou variáveis com nomes iguais mesmo em arquivos distintos. Namespace resolve esse problema.
 
 namespace Areas {
-  const PI = 3.14;
+  const pi2 = 3.14;
 
   export function circunferencia(raio: number): number {
-    return PI * Math.pow(raio, 2);
+    return pi2 * Math.pow(raio, 2);
   }
 
   export function retangulo(base: number, altura: number): number {
